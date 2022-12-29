cac8491fa78d28c67144783256cb47a6dcc03c4d
nosdeedson <josemariazumira@gmail.com>
2022-12-28 21:18:13 -0300

    first commit grpc

---
+++ README.md
@@ -0,0 +1,18 @@
+# Proto buffer (PROTOC)
+# COMMAND TO GENERATE THE "PROTO FILE"
+# protoc --go_out=. --go-grpc_out=. proto/course_category.proto 
+
+# Evans
+# INICIAR O Evans
+# evans -r repl
+# LISTAR SERVIÇOS
+# service
+# CHAMAR SERVIÇOS
+# call name
+# 
+# 
+# SQLITE
+# criar sqlilite db
+# sqlilite3 db.sqlilite
+# criar table 
+# create table categories (id string, name string, description string);
---
+++ go_grpc/cmd/grpcserver/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"go_grpc/internal/database"
+	"go_grpc/internal/pb"
+	"go_grpc/internal/service"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	fmt.Println("hello world")
+	db, err := sql.Open("sqlite3", "./db.sqlite")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	categoryDb := database.NewCategory(db)
+	categoryService := service.NewCategoryService(*categoryDb)
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterCategoryServiceServer(grpcServer, categoryService)
+	reflection.Register(grpcServer)
+
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		panic(err)
+	}
+
+	if err := grpcServer.Serve(lis); err != nil {
+		panic(err)
+	}
+}

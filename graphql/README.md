7b2fcba68aaab77278572e95dc4978ce282394d3
nosdeedson <josemariazumira@gmail.com>
2023-01-03 22:11:54 -0300

    graphql

---
+++ graphql/README.md
@@ -0,0 +1,2 @@
+# comando para gerar os arquivos
+#  go run github.com/99designs/gqlgen generate
---
+++ graphql/internal/database/course.go
@@ -0,0 +1,54 @@
+package database
+
+import (
+	"database/sql"
+
+	"github.com/google/uuid"
+	_ "github.com/google/uuid"
+)
+
+type Course struct {
+	db          *sql.DB
+	ID          string
+	Name        string
+	Description string
+	CategoryID  string
+}
+
+func NewCourse(db *sql.DB) *Course {
+	return &Course{db: db}
+}
+
+func (c *Course) Create(name, description, categroyID string) (*Course, error) {
+	id := uuid.New().String()
+
+	_, err := c.db.Exec("INSERT INTO courses (id, description, name, category_id) VALUES ($1, $2, $3, $4)", id, description, name, categroyID)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &Course{
+		ID:          id,
+		Description: description,
+		Name:        name,
+		CategoryID:  categroyID,
+	}, nil
+}
+
+func (c *Course) FindAll() ([]Course, error) {
+	rows, err := c.db.Query("SELECT id, name, description, category_id FROM courses")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	courses := []Course{}
+	for rows.Next() {
+		var id, name, description, categoryID string
+		if err := rows.Scan(&id, &name, &description, &categoryID); err != nil {
+			return nil, err
+		}
+		courses = append(courses, Course{ID: id, Name: name, Description: description, CategoryID: categoryID})
+	}
+	return courses, nil
+}
